0aa0cddf56635ded4b7edc6076739f1c6d642a9d
Jay <109895776+JavadEmami98@users.noreply.github.com>
2023-08-01 01:59:25 +0330

    Update Step6.jsx

--- Step6/Step6.jsx
+++ Step6/Step6.jsx
@@ -5,20 +5,20 @@ function Step6() {
   const isLg = useMediaQuery("(min-width:1024px)");
   return (
     <div>
-      <div className="items-center my-[50px]  lg:mb-[15px] py-[20px] lg:px-[44px] px-[8px] lg:mx-9 mx-4 border border-[#707070] rounded-[8px] lg:border lg:border-[#707070] lg:rounded-[12px] leading-[1.75]">
+       <div className="items-center my-[50px]  lg:mb-[15px] py-[20px] lg:px-[44px] px-[8px] lg:mx-9 mx-4 border border-[#707070] rounded-[8px] lg:border lg:border-[#707070] lg:rounded-[12px] leading-[1.75]">
         <div className="flex relative justify-center items-center">
-          <div className=" z-0 flex flex-col justify-center lg:mt-[200px] md:mt-[100px] mt-[100px] lg:mx-0 mx-2 lg:pr-[30px] pr-[20px] text-[#0A0A0A] w-full bg-[#656BD1] lg:rounded-[18px] rounded-[12px] lg:h-[193px] h-[83px] lg:ml-[100px] ml-[100px]">
+          <div className=" z-0 flex flex-col justify-center lg:mt-[200px] md:mt-[100px] mt-[100px] lg:mx-0 mx-2 lg:pr-[30px] pr-[20px] text-[#0A0A0A] w-full bg-[#F89833] lg:rounded-[18px] rounded-[12px] lg:h-[193px] h-[83px] lg:ml-[100px] ml-[100px]">
             <h3 className="lg:text-[40px] text-[16px] font-bold mb-[10px]">
-              قدم پنجم:
+              قدم ششم:
             </h3>
             <h3 className="lg:text-[30px] text-[12px] leading-7 text-[#F9F9F9]">
-              ارسال مدارک
+              شارژ کیف پول
             </h3>
           </div>
           {isLg ? (
             <div className="absolute top-0 left-0 z-10">
               <img
-                src="/images/mobile6.svg"
+                src="/images/mobile7.svg"
                 alt=""
                 loading="lazy"
                 className="object-cover w-full z-10"
@@ -27,7 +27,7 @@ function Step6() {
           ) : (
             <div className="absolute top-0 left-5 z-10">
               <img
-                src="/images/mobile6s.svg"
+                src="/images/mobile7s.svg"
                 alt=""
                 loading="lazy"
                 className="object-cover h-[261px] w-full z-10"
@@ -36,7 +36,9 @@ function Step6() {
           )}
         </div>
         <div className="lg:pr-[30px] lg:mx-0 lg:px-0 mx-3 px-[15px] lg:text-[25px] md:text-[15px] text-[13px] my-[15px] text-[#0A0A0A] leading-[1.85] mt-[75px]">
-          <span>مدارک شما بعد از بررسی کارشناسان به بانک ارسال می شود.</span>
+          <span>
+         - بررسی و تایید مدارک و فرم ها توسط بانک و شارژ کیف پول.
+          </span>
         </div>
       </div>
     </div>
