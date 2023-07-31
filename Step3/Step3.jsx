a9e382968ba9bbac0fcc2b607bc327a690c7ea28
Jay <109895776+JavadEmami98@users.noreply.github.com>
2023-08-01 01:57:02 +0330

    Update Step3.jsx

--- Step3/Step3.jsx
+++ Step3/Step3.jsx
@@ -5,26 +5,29 @@ function StepThree() {
   const isLg = useMediaQuery("(min-width:1024px)");
   return (
     <div>
-      <div className="items-center my-[50px]  lg:mb-[15px] py-[20px] lg:px-[44px] px-[8px] lg:mx-9 mx-4 border border-[#707070] rounded-[8px] lg:border lg:border-[#707070] lg:rounded-[12px] leading-[1.75]">
+ <div className="items-center my-[50px]  lg:mb-[15px] py-[20px] lg:px-[44px] px-[8px] lg:mx-9 mx-4 border border-[#707070] rounded-[8px] lg:border lg:border-[#707070] lg:rounded-[12px] leading-[1.75]">
         <div className="flex relative justify-center items-center">
-          <div className=" z-0 flex flex-col justify-center lg:mt-[200px] md:mt-[100px] mt-[100px] lg:mx-0 mx-2 lg:pr-[30px] pr-[20px] text-[#0A0A0A] w-full bg-[#359DD5] lg:rounded-[18px] rounded-[12px] lg:h-[193px] h-[83px] lg:ml-[100px] ml-[100px]">
+          <div className=" z-0 flex flex-col justify-center lg:mt-[200px] md:mt-[100px] mt-[100px] lg:mx-0 mx-2 lg:pr-[30px] pr-[20px] text-[#F89833] w-full bg-[#101010] lg:rounded-[18px] rounded-[12px] lg:h-[193px] h-[83px] lg:ml-[100px] ml-[100px]">
             <h3 className="lg:text-[40px] text-[16px] font-bold mb-[10px]">
-              لیست مدارک مورد نیاز:
+              قدم سوم:
+            </h3>
+            <h3 className="lg:text-[30px] text-[12px] leading-7 text-[#F9F9F9]">
+              پیش ثبت نام آنلاین یا تحویل حضوری مدارک و ثبت نام
             </h3>
           </div>
           {isLg ? (
             <div className="absolute top-0 left-0 z-10">
               <img
-                src="/images/mobile3.svg"
+                src="/images/mobile4.svg"
                 alt=""
                 loading="lazy"
                 className="object-cover w-full z-10"
               />
             </div>
           ) : (
-            <div className="absolute top-0 left-5 z-10">
+            <div className="absolute top-0 left-3 z-10">
               <img
-                src="/images/mobile3s.svg"
+                src="/images/mobile4s.svg"
                 alt=""
                 loading="lazy"
                 className="object-cover h-[261px] w-full z-10"
@@ -33,79 +36,65 @@ function StepThree() {
           )}
         </div>
         <div className="lg:pr-[30px] lg:mx-0 lg:px-0 mx-3 px-[15px] lg:text-[25px] md:text-[15px] text-[13px] my-[15px] text-[#0A0A0A] leading-[1.85] mt-[75px]">
-          <h3 className=" font-bold">مدارک شناسایی:</h3>
-          <span>
-            شناسنامه به همراه کپی از تمام صفحات
-            <br />
-            کارت ملی به همراه کپی از پشت و رو کارت
-            <br />
-            کارت پایان خدمت به همراه کپی از پشت و رو کارت (برگه معافیت مورد قبول
-            است)
-          </span>
-          <h3 className="mt-[20px]  font-bold">مدارک شغلی:</h3>
-          <span>
-            با توجه به دسته بندی زیر، صنف کاری خود رامشخص کنید و مدارک مورد نیاز
-            همان شغل را تهیه کنید .
-          </span>
-          <h3 className="mt-[20px]  text-[#707070] font-bold">
-            کارمندان دولتی :
-          </h3>
-          <span>
-            حکم کارگزینی یا گواهی اشتغال به کار
-            <br />
-            فیش حقوقی یا پرینت سوابق بیمه
-          </span>
-          <h3 className="mt-[20px]  text-[#707070] font-bold">
-            کارمندان بخش خصوصی :
-          </h3>
-          <span> گواهی اشتغال به کار</span>
-          <br />
-          <span> فیش حقوقی</span>
-          <br />
-          <span>پرینت سوابق بیمه</span>
-          <h3 className="mt-[20px]  text-[#707070] font-bold">
-            کارمندان نظامی :
-          </h3>
-          <span>حکم کارگزینی یا گواهی اشتغال به کار</span>
-          <h3 className="mt-[20px]  text-[#707070] font-bold">بازنشستگان :</h3>
-          <span> فیش حقوقی</span>
-          <br />
-          <span>آخرین حکم افزایش مستمری</span>
-          <h3 className="mt-[20px]  text-[#707070] font-bold">مشاغل آزاد :</h3>
-          <span> جواز کسب</span>
-          <br />
-          <span>
-            (کسبه ای که جواز کسب ندارند اجاره نامه یا سند ملک تجاری که در آن
-            فعالیت می کنند به همراه برگه استشهادیه
-            <br />
-            که از سازمان ما دریافت کرده اند را تکیل کنند و تحویل باجه دهنده)
-          </span>
-          <h3 className="mt-[20px]  text-[#707070] font-bold">
-            فروشندگان و کارمندان کسبه:
-          </h3>
-          <span>
-            گواهی اشتغال به کار در سربرگ سازمان با مهر معتبر آن سازمان.
+          <span className="text-[25px] font-bold text-[#707070]">
+            پیش ثبت نام آنلاین
           </span>
           <br />
           <span>
-            ( احتمالا خیلی از مشاغل در این دسته بندی قرار نمی گیرند شما باید
-            برای تشکیل پرونده یک مدرک شغلی معتبر
+            در این مرحله شما می توانید به صورت آنلاین ثبت نام اولیه کنید.
             <br />
-            به بانک ارائه بدید و فرقی ندارد که شغل شما در کدام دسته بندی قرار
-            دارد به عنوان مثل رانندگان تاکسیرانی کپی
+            پرداخت مبلغ 75 هزار تومان بابت هزینه پوشه و فرم ها و قرارداد بانک.
             <br />
-            سند ماشین با کارت اتحادیه تاکسیرانی میارن)
+            مراجعه حضوری و تحویل فرم ها و مدارک مورد نیاز به باجه تسهیلات.
+            <br />
           </span>
-          <h3 className="mt-[20px]   text-[#290101] font-bold">
-            مدارک محل سکونت :
-          </h3>
-          <div className="mb-[20px]">
+          <div className="mt-[20px]">
+            <Button
+              variant="outlined"
+              sx={{
+                height: {
+                  lg: "76px !important",
+                  md: "38px !important",
+                  sm: "32px !imporntant",
+                },
+                fontSize: {
+                  lg: "21px !important",
+                  md: "10px !important",
+                  sm: "10px !important",
+                },
+                p: "10px !important",
+                border: "2px solid #F89833",
+                mb: "20px",
+                color: "#F89833",
+              }}
+            >
+              مشاهده آموزش پیش ثبت نام آنلاین
+            </Button>
+          </div>
+          <div className="lg:text-[25px] md:text-[15px] text-[13px] my-[15px] text-[#0A0A0A] leading-[1.85] mt-[25px]">
+            <span className="text-[25px] font-bold text-[#707070]">
+              {" "}
+              ثبت نام حضوری{" "}
+            </span>
+            <br />
             <span>
-              اگه منزل شما اجاره است، اجاره نامه با تائیدیه کدپستی، اگه ملک شخصی
-              است، سند با تائیدیه کدپستی.
+              - در این مرحله باید مدارک خود را به صورت حضوری تحویل باجه دهید.
               <br />
-              (صرفا به علت احراز هویت محل سکونت شماست و سند ملک شما در رهن یا
-              ضمانت بانک قرار نمیگیره)
+              - مراحل ثبت نام شما بعد از صحت سنجی مدارک به توسط متصدی باجه
+              تسهیلات انجام می شود.
+              <br />
+              - پرداخت مبلغ 75 هزار تومان بابت هزینه پوشه و فرم های قرارداد
+              بانک.
+              <br />
+              - پرداخت مبلغ 10/6% به عنوان هزینه خدمات و زیر ساخت.
+              <br />- تعیین نوبت و مراجعه به غرفه تسهیلات برای تکمیل فرم های
+              بانکی.
+            </span>
+          </div>
+          <div className="mt-[20px] md:text-[23px] text-[30px] text-[#290101]  leading-[1.85]">
+            <h3>آدرس مراجعه حضوری :</h3>
+            <span className="text-[#707070]">
+              - خیابان جمهوری، تقاطع حافظ، پاساژ چارسو، طبقه منفی 1، غرفه A62
             </span>
           </div>
         </div>
@@ -124,10 +113,12 @@ function StepThree() {
                 sm: "10px !important",
               },
               p: "10px !important",
-              border: "2px solid #3E81E6",
+              border: "2px solid #F89833",
+              mb: "20px",
+              color: "#F89833",
             }}
           >
-            دریافت لیست مدارک
+            نمایش آدرس بر روی نقشه
           </Button>
         </div>
       </div>
